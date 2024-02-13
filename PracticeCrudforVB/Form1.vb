7491ae8703e0270aeb22d7cd6d43b09db5a54f14
Yoshibaits <neil_dsg@yahoo.com>
2024-02-13 09:51:13 +0800

    Arranged the positioning of functions

--- PracticeCrudforVB/Form1.vb
+++ PracticeCrudforVB/Form1.vb
@@ -12,7 +12,41 @@ Public Class Form1
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
         delete()
     End Sub
+    Private Sub btn_update_Click(sender As Object, e As EventArgs) Handles btn_update.Click
+        Edit()
+    End Sub
 
+    Private Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
+        clear()
+    End Sub
+
+    Private Sub save()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("INSERT INTO `tbl_crud`(`PRODUCTNO`, `PRODUCTNAME`, `PRICE`, `TYPEOFGROUP`, `EXPDATE`, `STATUS`) 
+            VALUES (@PRODUCTNO,@PRODUCTNAME,@PRICE,@TYPEOFGROUP,@EXPDATE,@STATUS)", conn)
+            cmd.Parameters.Clear()
+            cmd.Parameters.AddWithValue("@PRODUCTNO", txt_prodno.Text)
+            cmd.Parameters.AddWithValue("@PRODUCTNAME", txt_prodname.Text)
+            cmd.Parameters.AddWithValue("@PRICE", CDec(txt_price.Text))
+            cmd.Parameters.AddWithValue("@TYPEOFGROUP", combo_prodGroup.Text)
+            cmd.Parameters.AddWithValue("@EXPDATE", CDate(exp_datePicker.Text))
+            cmd.Parameters.AddWithValue("@STATUS", CBool(status_checkbox.Checked.ToString))
+
+            i = cmd.ExecuteNonQuery
+            If i > 0 Then
+                MessageBox.Show("Record Save Success !", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Record Save Failed !", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+        clear()
+    End Sub
     Public Sub delete()
         If MsgBox("Are you sure you want to delete this record?") Then
             Try
@@ -46,55 +80,6 @@ Public Class Form1
 
     End Sub
 
-    Public Sub DGV_load()
-        DataGridView1.Rows.Clear()
-        Try
-            conn.Open()
-            Dim cmd As New MySqlCommand("SELECT * FROM tbl_crud", conn)
-            dr = cmd.ExecuteReader
-            While dr.Read
-                DataGridView1.Rows.Add(dr.Item("PRODUCTNO"), dr.Item("PRODUCTNAME"), dr.Item("PRICE"), dr.Item("TYPEOFGROUP"), dr.Item("EXPDATE"), Format(CBool(dr.Item("STATUS"))))
-            End While
-            dr.Dispose()
-        Catch ex As Exception
-            MsgBox(ex.Message)
-        Finally
-            conn.Close()
-        End Try
-    End Sub
-
-    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
-        save()
-        DGV_load()
-    End Sub
-
-    Private Sub save()
-        Try
-            conn.Open()
-            Dim cmd As New MySqlCommand("INSERT INTO `tbl_crud`(`PRODUCTNO`, `PRODUCTNAME`, `PRICE`, `TYPEOFGROUP`, `EXPDATE`, `STATUS`) 
-            VALUES (@PRODUCTNO,@PRODUCTNAME,@PRICE,@TYPEOFGROUP,@EXPDATE,@STATUS)", conn)
-            cmd.Parameters.Clear()
-            cmd.Parameters.AddWithValue("@PRODUCTNO", txt_prodno.Text)
-            cmd.Parameters.AddWithValue("@PRODUCTNAME", txt_prodname.Text)
-            cmd.Parameters.AddWithValue("@PRICE", CDec(txt_price.Text))
-            cmd.Parameters.AddWithValue("@TYPEOFGROUP", combo_prodGroup.Text)
-            cmd.Parameters.AddWithValue("@EXPDATE", CDate(exp_datePicker.Text))
-            cmd.Parameters.AddWithValue("@STATUS", CBool(status_checkbox.Checked.ToString))
-
-            i = cmd.ExecuteNonQuery
-            If i > 0 Then
-                MessageBox.Show("Record Save Success !", "CRUD", MessageBoxButtons.OK, MessageBoxIcon.Information)
-            Else
-                MessageBox.Show("Record Save Failed !", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
-            End If
-
-        Catch ex As Exception
-            MsgBox(ex.Message)
-        Finally
-            conn.Close()
-        End Try
-        clear()
-    End Sub
     Public Sub clear()
 
         txt_prodno.Clear()
@@ -105,19 +90,6 @@ Public Class Form1
         status_checkbox.CheckState = False
 
     End Sub
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
-        txt_prodno.Text = DataGridView1.CurrentRow.Cells(0).Value
-        txt_prodname.Text = DataGridView1.CurrentRow.Cells(1).Value
-        txt_price.Text = DataGridView1.CurrentRow.Cells(2).Value
-        combo_prodGroup.Text = DataGridView1.CurrentRow.Cells(3).Value
-        exp_datePicker.Text = DataGridView1.CurrentRow.Cells(4).Value
-        status_checkbox.Checked = DataGridView1.CurrentRow.Cells(5).Value
-
-        txt_prodno.ReadOnly = True
-        btn_save.Enabled = False
-
-    End Sub
-
     Sub Edit()
         Try
             conn.Open()
@@ -145,13 +117,41 @@ Public Class Form1
         clear()
         DGV_load()
     End Sub
-
-    Private Sub btn_update_Click(sender As Object, e As EventArgs) Handles btn_update.Click
-        Edit()
+    Public Sub DGV_load()
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            Dim cmd As New MySqlCommand("SELECT * FROM tbl_crud", conn)
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("PRODUCTNO"), dr.Item("PRODUCTNAME"), dr.Item("PRICE"), dr.Item("TYPEOFGROUP"), dr.Item("EXPDATE"), Format(CBool(dr.Item("STATUS"))))
+            End While
+            dr.Dispose()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
     End Sub
 
-    Private Sub btn_clear_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
-        clear()
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        save()
+        DGV_load()
+    End Sub
+
+
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        txt_prodno.Text = DataGridView1.CurrentRow.Cells(0).Value
+        txt_prodname.Text = DataGridView1.CurrentRow.Cells(1).Value
+        txt_price.Text = DataGridView1.CurrentRow.Cells(2).Value
+        combo_prodGroup.Text = DataGridView1.CurrentRow.Cells(3).Value
+        exp_datePicker.Text = DataGridView1.CurrentRow.Cells(4).Value
+        status_checkbox.Checked = DataGridView1.CurrentRow.Cells(5).Value
+
+        txt_prodno.ReadOnly = True
+        btn_save.Enabled = False
+
     End Sub
 
     Private Sub txt_search_TextChanged(sender As Object, e As EventArgs) Handles txt_search.TextChanged
